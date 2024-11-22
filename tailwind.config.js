0ec8ae5f81525e0e7503db6d2bd7aca44c050ba8
hamipirzada <hamipirzada@gmail.com>
2024-11-23 01:47:10 +0530

    Update workflow to build TailwindCSS and add necessary configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js",
+    "*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
